02e96f10c64b1e887b3ed8749d87e48027cbbb09
soicchi <soicchi@icloud.com>
2024-11-03 00:30:29 +0900

    fix: fixed metadata

--- app/infrastructure/postgres/database/schema.go
+++ app/infrastructure/postgres/database/schema.go
@@ -1,11 +1,19 @@
 package database
 
 import (
+	"time"
+
 	"github.com/google/uuid"
 )
 
-type Todo struct {
+type Base struct {
 	ID uuid.UUID `gorm:"type:uuid;primaryKey"`
+	CreatedAt time.Time `gorm:"type:timestamp;not null"`
+	UpdatedAt time.Time `gorm:"type:timestamp;not null"`
+}
+
+type Todo struct {
+	Base `gorm:"embedded"`
 	Title string `gorm:"type:varchar(255)"`
 	Description string `gorm:"type:text"`
 	StatusID uuid.UUID `gorm:"type:uuid"`
@@ -13,13 +21,13 @@ type Todo struct {
 }
 
 type Status struct {
-	ID uuid.UUID `gorm:"type:uuid;primaryKey"`
-	State string `gorm:"type:varchar(64);unique"`
+	Base `gorm:"embedded"`
+	State string `gorm:"type:varchar(64);unique;not null"`
 	Todos []Todo `gorm:"foreignKey:StatusID"`
 }
 
 type Priority struct {
-	ID uuid.UUID `gorm:"type:uuid;primaryKey"`
-	Level string `gorm:"type:varchar(32);unique"`
+	Base `gorm:"embedded"`
+	Level string `gorm:"type:varchar(32);unique;not null"`
 	Todos []Todo `gorm:"foreignKey:PriorityID"`
 }
